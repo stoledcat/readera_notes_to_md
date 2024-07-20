30483f2d4fb92c5b759914eafa10a73a288a2275
stoledcat <jetnavo@yandex.ru>
2024-07-20 16:14:11 +0300

    Первый рализ. Пока работает выгрузка всех цитат без запроса ввода даты отсечки

--- main.py
+++ main.py
@@ -15,22 +15,27 @@ def convert_time(year=1970, month=1, day=3, hour=0, min=0, sec=0):
     return milliseconds
 
 
-def write_file():
+def open_file():
+    with open("library.json", "r", encoding="utf-8") as input_file:
+        data = json.load(input_file)
+        return data
+
+
+def write_file(doc_title, citations):
     with open(doc_title, "w", encoding="utf-8") as output_file:
         output_file.write(citations)
 
 
 def main():
-    with open("library.json", "r", encoding="utf-8") as file:
-        data = json.load(file)
-    data_string = data["docs"]
+    data_string = open_file()["docs"]  # открыть исходный файл
     for item in data_string:
         citations = ""
         if item["citations"] != []:
-            doc_title = f'{item["data"]["doc_title"]}.md'
+            doc_title = f'{item["data"]["doc_title"]}.md'  # название книги => имя выходного файла
             for citation in item["citations"]:
                 if convert_time() < citation["note_insert_time"]:
                     citations += f'>{citation["note_body"]}\n'
+            write_file(doc_title, citations)
 
 
 if __name__ == "__main__":
