1c4bd710bb648f0c5eb48409acbebd2ab7a3ff36
stoledcat <jetnavo@yandex.ru>
2024-07-23 21:59:16 +0300

    Добавлен фильтр поиска цитат по дате

--- main.py
+++ main.py
@@ -10,31 +10,50 @@ import zipfile
 """
 
 
-# конвертировать указанное время в миллисекунды
-def convert_time(year=1970, month=1, day=3, hour=0, min=0, sec=0):
-    milliseconds = int(
-        datetime.datetime(year, month, day, hour, min, sec).timestamp() * 1000
-    )
-    return milliseconds
-
-#
+# указать название метафайла
 def select_metafile():
-    print("Выберите название метафайла,")
-    metafile = input("на который будут ссылаться все цитаты: ")
+    # print("Выберите название метафайла,")
+    metafile = input("Выберите название метафайла,\
+на который будут ссылаться все цитаты: ")
     return metafile
 
 
-# # запросить начальную дату экспорта цитат
-# def enter_date():
-#     print("Укажите, с какой даты экспортировать цитаты.")
-#     choose = input("Либо введите '0', чтобы экспортировать все: ")
-#     if int(choose) != 0:
-#         year = int(input("Введите год: "))
-#         month = input("Введите месяц: ")
-#         month = int(month.lstrip("0"))
-#         day = input("Введите день: ")
-#         day = int(day.lstrip("0"))
-#     return convert_time(year, month, day)
+# запросить начальную дату экспорта цитат
+def enter_date():
+    print("Экспортировать все цитаты - 0")
+    print("Экспортировать начиная с указанной даты - 1")
+    choose = input("Выбор: ")
+    while choose not in ("0", "1") or choose.isalpha():
+        choose = input("Неверный вариант. Повторите выбор: ")
+    if choose == "1":
+        year = input("Введите год: ")
+        while not year.isdigit():
+            year = input("Некорректное значение. Укажите год: ")
+        year = int(year)
+        month = input("Введите месяц: ")
+        while not month.isdigit() or int(month) > 12:
+            month = input("Некорректное значение. Укажите месяц: ")
+        month = int(month.lstrip("0"))
+        day = input("Введите день: ")
+        while not day.isdigit() or int(day) > 31:
+            day = input("Некорректное значение. Укажите день: ")
+        day = int(day.lstrip("0"))
+        check_value = int(year) + int(month) + int(day)
+        if not str(check_value).isdigit():
+            print("Введены некорретные данные.")
+    elif choose == "0":
+        year = 1970
+        month = 1
+        day = 3
+    return convert_time(year, month, day)
+
+
+# конвертировать указанное время в миллисекунды
+def convert_time(year=1970, month=1, day=3):
+    milliseconds = int(
+        datetime.datetime(year, month, day).timestamp() * 1000
+    )
+    return milliseconds
 
 
 # подготовить бэкап, изменить расширение bak на zip
@@ -101,11 +120,11 @@ def replace_symbols(item):
 
 
 # создать хранилище с цитатами
-def make_citations(item):
+def make_citations(item, timestamp):
     count = 0
     citations = ""
     for citation in item["citations"]:
-        if convert_time() < citation["note_insert_time"]:
+        if timestamp < citation["note_insert_time"]:
             count += 1
             citations += f'>{citation["note_body"]}\n\n'
     if count >= 1:
@@ -126,7 +145,7 @@ def make_books(book_key, doc_title, collection, citations, metafile):
 
 
 def main():
-    # enter_date()
+    timestamp = enter_date()
     metafile = select_metafile()
     data_docs = extract_from_zip()["docs"]  # получить данные файлов
     data_colls = extract_from_zip()  # получить данные коллекций
@@ -136,7 +155,7 @@ def main():
         if item["data"]["doc_format"] in ("FB2", "EPUB", "MOBI"):
             if item["citations"] != []:
                 doc_title = replace_symbols(item)
-                citations = make_citations(item)
+                citations = make_citations(item, timestamp)
                 make_books(book_key, doc_title, collection, citations,
                            metafile)
 
